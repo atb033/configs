898f18c4c193244d50f58d256d459bbce4b3438b
atb033 <ashwinbose123@gmail.com>
2019-11-04 11:39:54 +0100

    Add vscode

--- system_essentials.sh
+++ system_essentials.sh
@@ -5,3 +5,8 @@ sudo apt-get install tmux
 # Fuzzy finder
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+# PlotJuggler
+
+# VS Code
+sudo snap install code --classic
