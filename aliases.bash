8e9cc6ee5f5301a1d3657f6a460e54e440a480f1
Ashwin Bose <ashwin@toadi.com>
2021-03-31 16:21:31 +0200

    Some alias functions

--- aliases.bash
+++ aliases.bash
@@ -7,8 +7,9 @@ toadi-remote-cleanup () {
     if [ -z $ROBOT_ID ]; then
         echo "Please enter the robot id"
     else
-        scp ~/toadi-main/sdk/jetson/linux/lib/libSlamCore.so $toadi:/home/toadi/data/toadi/lib
-        scp ~/toolchain/tc-jetson-nano/aarch64-poky-linux/usr/lib/libboost_system.so* $toadi:/home/toadi/data/toadi/lib
+	ssh $toadi mkdir -p /home/toadi/data/toadi/lib \
+        && scp ~/toadi-main/sdk/jetson/linux/lib/libSlamCore.so $toadi:/home/toadi/data/toadi/lib \
+        && scp ~/toolchain/tc-jetson-nano/aarch64-poky-linux/usr/lib/libboost_system.so* $toadi:/home/toadi/data/toadi/lib
     fi
 }
 
@@ -22,8 +23,19 @@ toadi-get-sdk () {
         cd ~/toadi-main/sdk
         git remote add toadi-sdk ssh://git@10.55.0.177:10022/toadi/toadi-sdk.git
         git fetch toadi-sdk
-        git checkout toadi-sdk/$COMMIT_HASH
+	git reset --hard
+        git checkout toadi-sdk/sdk-$COMMIT_HASH
         git reset --hard
         cd -
+    fi
+}
+
+toadi-find-string-in-files () {
+    SEARCH_STRING=$1
+    FILE_NAME_STRING=$2
+    if [ -z $FILE_NAME_STRING ]; then
+        echo "Syntax: toadi-find-string-in-files <string-to-search> <part-of-filename>"
+    else
+        find . -name "*$FILE_NAME_STRING*" -exec grep -l $SEARCH_STRING {} +
     fi
 }
